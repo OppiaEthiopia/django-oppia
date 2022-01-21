8639e3273990f91c5295912bbfdf6968e79f238d
Joseba S <jjoseba@gmail.com>
2022-01-21 17:58:51 +0100

    Filter in the admin pages by username

--- reports/admin.py
+++ reports/admin.py
@@ -7,6 +7,7 @@ from reports.models import DashboardAccessLog
 
 class DashboardAccessLogAdmin(admin.ModelAdmin):
     list_display = ('user', 'access_date', 'url', 'ip', 'data')
+    search_fields = ['user__username']
 
 
 admin.site.register(DashboardAccessLog, DashboardAccessLogAdmin)
